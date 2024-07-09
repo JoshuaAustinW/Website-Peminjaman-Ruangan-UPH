0e0cc57f7fffae71a5e42ec72a89c94fab3adfe4
Joshua Austin <joshuaaustin789@gmail.com>
2024-07-09 09:41:43 +0700

    w

--- Website/HomePage/fungsi.js
+++ Website/HomePage/fungsi.js
@@ -109,7 +109,7 @@ function ButtonPinjam3(){
 
 function ButtonDetails1(){
     let nomorruangan = document.getElementById('txt0').innerHTML;
-    let IndexRuanganTerpilih = filteredRuangan.findIndex(room => room.number === nomorruangan.replace("ROOM-",""));
+    let IndexRuanganTerpilih = filteredRuangan.findIndex(room => room.no === nomorruangan.replace("ROOM-",""));
     let tiperuangan = filteredRuangan[IndexRuanganTerpilih].tipe;
     let kapasitasruangan = filteredRuangan[IndexRuanganTerpilih].kapasitas;
     let lokasiruangan = filteredRuangan[IndexRuanganTerpilih].lokasi;
@@ -124,7 +124,7 @@ function ButtonDetails1(){
 
 function ButtonDetails2(){
     let nomorruangan = document.getElementById('txt1').innerHTML;
-    let IndexRuanganTerpilih = filteredRuangan.findIndex(room => room.number === nomorruangan.replace("ROOM-",""));
+    let IndexRuanganTerpilih = filteredRuangan.findIndex(room => room.no === nomorruangan.replace("ROOM-",""));
     let tiperuangan = filteredRuangan[IndexRuanganTerpilih].tipe;
     let kapasitasruangan = filteredRuangan[IndexRuanganTerpilih].kapasitas;
     let lokasiruangan = filteredRuangan[IndexRuanganTerpilih].lokasi;
@@ -139,7 +139,7 @@ function ButtonDetails2(){
 
 function ButtonDetails3(){
     let nomorruangan = document.getElementById('txt2').innerHTML;
-    let IndexRuanganTerpilih = filteredRuangan.findIndex(room => room.number === nomorruangan.replace("ROOM-",""));
+    let IndexRuanganTerpilih = filteredRuangan.findIndex(room => room.no === nomorruangan.replace("ROOM-",""));
     let tiperuangan = filteredRuangan[IndexRuanganTerpilih].tipe;
     let kapasitasruangan = filteredRuangan[IndexRuanganTerpilih].kapasitas;
     let lokasiruangan = filteredRuangan[IndexRuanganTerpilih].lokasi;
